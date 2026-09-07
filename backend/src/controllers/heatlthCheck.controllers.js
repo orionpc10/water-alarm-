704e3afe883d7f1449603ae8b6fe77ef9baf4263
orionpc10 <aaryanpc10@gmail.com>
2026-09-07 20:42:45 +0530

    add healthcheck

---
+++ backend/src/controllers/heatlthCheck.controllers.js
@@ -0,0 +1,8 @@
+import {apiResponse} from '../utils/apiResponse.js'
+import {asyncHandler} from '../utils/asyncHandler.js'
+
+const healthCheck = asyncHandler(async (req, res, next) =>{
+    return res.status(200).json(new apiResponse(200, "OK", "Health check passed"))
+})
+
+export {healthCheck}
